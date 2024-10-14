5e442a09aa870bde294a76df912d711c3cec7a14
Alexander Mishurov <al.mishurov@gmail.com>
2024-10-14 19:31:58 +0300

    add torch

--- main.py
+++ main.py
@@ -43,6 +43,20 @@ def nope_function(task):
 
 
 def main():
+    import os
+
+    # Needs to be done before numpy is imported!
+    os.environ['OPENBLAS_NUM_THREADS'] = '1'
+    os.environ["OMP_NUM_THREADS"] = '1'
+    os.environ["MKL_NUM_THREADS"] = '1'
+    os.environ["VECLIB_MAXIMUM_THREADS"] = '1'
+    os.environ["NUMEXPR_NUM_THREADS"] = '1'
+
+    import torch  # noqa: E402
+
+    torch.set_num_interop_threads(1)
+
+
     jobs = [delayed(nope_function)(i) for i in range(8)]
     with tqdm_joblib(tqdm(desc="debug joblib", total=len(jobs))):
         Parallel(n_jobs=8)(jobs)
