e0f554d66e28c99a25d69f2dc3dc4fea21521299
Alexander Mishurov <al.mishurov@gmail.com>
2024-10-14 17:05:30 +0300

    run clearml

---
+++ run_clearml.py
@@ -0,0 +1,14 @@
+import clearml
+
+project = 'Demo'
+task_name = 'joblib zomnie'
+docker_image = 'registry.pyn.ru/python3.9-kardinal-clearml:2024.03.11'
+repo = 'git@github.com:/hhru/amishurov/joblib_zomnie'
+
+task = clearml.Task.init(project_name=project, task_name=task_name)
+task.set_base_docker(
+    docker_image=docker_image,
+)
+task.set_repo(repo=repo, branch='master')
+task.set_script(entry_point='main.py')
+task.execute_remotely(queue_name='default')
